9bccc3887fb37e00bf3cc5fa4f8be1d15ef632d0
Nina Große <nina.grosse@posteo.de>
2024-12-21 17:43:43 +0100

    add settings, plugins & aliases for cli-tools

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -10,6 +10,10 @@ export HISTCONTROL=ignoreboth
 export HISTIGNORE="&:[bf]g:c:clear:history:exit:q:pwd:* --help"
 export DISABLE_AUTO_UPDATE=true
 
+# eza plugin settings
+zstyle ':omz:plugins:eza' 'dirs-first' yes
+zstyle ':omz:plugins:eza' 'icons' yes
+
 # oh-my-zsh plugins
 export plugins=(git
 history
@@ -18,8 +22,59 @@ rsync
 ssh
 zsh-autosuggestions
 zsh-syntax-highlighting
-systemadmin)
+systemadmin
+eza
+fzf
+zoxide)
 
 # load oh-my-zsh
 source $CURR_DIR/ohmyzsh/oh-my-zsh.sh
 autoload -U compinit && compinit
+
+# Set preferred editor for local and remote sessions
+# Use vscode when using it, otherwise use nano
+if [[ -n $TERM_PROGRAM ]]; then
+  if [[ $TERM_PROGRAM == 'vscode' ]]; then
+    export EDITOR='code --wait'
+  else
+    export EDITOR='nano'
+  fi
+else
+  export EDITOR='nano'
+fi
+
+# Alias to reload zsh (e.g. after editing .zshrc or editing plugins)
+alias src='exec zsh'
+
+# Init Starship prompt
+eval "$(starship init zsh)"
+
+# Replace cat with bat
+alias cat='bat'
+
+# More eza aliases
+alias l='ll' # make alias l the same as ll (eza -gl)
+alias lt='ls --tree' # tree alias (ls is already aliased to eza)
+alias lat='ls -a --tree' # tree alias including hidden folder/files
+
+# Alias to pipe output to fzf with 'command F' instead of 'command | fzf'
+alias -g F="| fzf"
+
+# Use bat & eza previews in fzf
+show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
+export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+  esac
+}
+
+# Replace cd with z (zoxide)
+alias cd="z"
